5e2af24c6315c7648a77bb691ff0ac9d083ce7f6
Abdul Waheed <thecaptain0078@gmail.com>
2022-08-20 20:20:44 +0500

    added a typescirpt fn

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,10 @@ import express, { Application, Request, Response, NextFunction } from "express";
 
 const app: express.Application = express();
 
+const add = () => {
+  (a: number, b: number): number => a + b;
+};
+
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
   res.send("Hello");
 });
